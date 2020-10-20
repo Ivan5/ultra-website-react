2030ccfb9a34ee106b242777103b0d7c08524b70
Ivan5 <converse_ivan@hotmail.com>
2020-10-20 17:48:10 -0500

    Make a Home page component

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,33 @@
+export const homeObjOne = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We help business owners increase their revenue. Out team of unique specialist cona help you acheive yout business goals",
+  headLine: "Lead Generation Specialist for Online Business",
+  lightText: true,
+  topLine: "Margeting Agency",
+  img: require("../../images/image1.svg"),
+  alt: "Image",
+  start: "",
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: "start",
+  lightTopLine: false,
+  lightTextDesc: false,
+  buttonLabel: "Get Started",
+  description:
+    "We help business owners increase their revenue. Out team of unique specialist cona help you acheive yout business goals",
+  headLine: "Lead Generation Specialist for Online Business",
+  lightText: false,
+  topLine: "Margeting Agency",
+  img: require("../../images/image1.svg"),
+  alt: "Image",
+  start: "true",
+};
